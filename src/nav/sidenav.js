f4d1fa66a82451c6b2e41780f7cee728ae19fc6a
hiddenbebb <cbj4133@naver.com>
2024-04-13 14:24:59 +0900

    sidebar 대략 기능 생성

--- src/nav/sidenav.js
+++ src/nav/sidenav.js
@@ -1,8 +1,47 @@
-import { React } from "react";
+import { React, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+
 import "./nav.css";
 
 function Sidenav() {
-  return <div className="side_nav"></div>;
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="side_nav">
+      <p className="side_1">인공지능</p>
+      <p className="side_2">강화학습</p>
+      <p
+        className="side_3"
+        onClick={() => setOpen(!open)}
+        aria-controls="심층강화학습"
+        aria-expanded={open}
+      >
+        심층강화학습
+      </p>
+      <Collapse in={open}>
+        <div>
+          <p
+            className="side_4"
+            id="심층강화학습"
+            onClick={() => setOpen(!open)}
+            aria-controls="심층강화학습"
+            aria-expanded={open}
+          >
+            Ch1.강화학습 개요
+          </p>
+          <p className="side_4" id="심층강화학습">
+            Ch2
+          </p>
+          <p className="side_4" id="심층강화학습">
+            Ch3
+          </p>
+          <p className="side_4" id="심층강화학습">
+            Ch4
+          </p>
+        </div>
+      </Collapse>
+    </div>
+  );
 }
 
 export default Sidenav;
