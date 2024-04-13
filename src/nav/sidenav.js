8623bf34dd96bb50fdf6398cf6b775ca960070c6
hiddenbebb <cbj4133@naver.com>
2024-04-13 20:45:41 +0900

    side nav 디자인 완료

--- src/nav/sidenav.js
+++ src/nav/sidenav.js
@@ -5,41 +5,49 @@ import Collapse from "react-bootstrap/Collapse";
 import "./nav.css";
 
 function Sidenav() {
-  const [open, setOpen] = useState(false);
+  // yolo
+  const [yoloOpen, setYoloOpen] = useState(false);
+
+  // 심층강화
+  const [simReinOpen, setSimReinOpen] = useState(false);
+
   return (
     <div className="side_nav">
-      <p className="side_1">인공지능</p>
-      <p className="side_2">강화학습</p>
-      <p
-        className="side_3"
-        onClick={() => setOpen(!open)}
-        aria-controls="심층강화학습"
-        aria-expanded={open}
-      >
-        심층강화학습
-      </p>
-      <Collapse in={open}>
-        <div>
-          <p
-            className="side_4"
-            id="심층강화학습"
-            onClick={() => setOpen(!open)}
-            aria-controls="심층강화학습"
-            aria-expanded={open}
-          >
-            Ch1.강화학습 개요
-          </p>
-          <p className="side_4" id="심층강화학습">
-            Ch2
-          </p>
-          <p className="side_4" id="심층강화학습">
-            Ch3
-          </p>
-          <p className="side_4" id="심층강화학습">
-            Ch4
-          </p>
+      <div className="side_main_title">Study</div>
+      <div className="title">
+        <div className="title1">인공지능</div>
+        <div className="title">
+          <div className="title2">컴퓨터 비젼</div>
+          <div className="title title3">
+            YOLO v8
+            <button onClick={() => setSimReinOpen(!simReinOpen)}>⬇</button>
+            <Collapse in={simReinOpen}>
+              <div>
+                <div className="title title4">Yolo param</div>
+              </div>
+            </Collapse>
+          </div>
         </div>
-      </Collapse>
+        <br />
+        <div className="title">
+          <div className="title2">강화 학습</div>
+          <div className="title title3">
+            심층강화학습
+            <button onClick={() => setYoloOpen(!yoloOpen)}>⬇</button>
+            <Collapse in={yoloOpen}>
+              <div>
+                <div className="title title4">Ch1.강화학습 개요</div>
+                <div className="title title4">
+                  Ch2. 마르코프 <br />
+                  &emsp;&emsp;&emsp;결정 프로세스
+                </div>
+                <div className="title title4">Ch3. 동적 프로그래밍</div>
+                <div className="title title4">Ch4</div>
+              </div>
+            </Collapse>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
