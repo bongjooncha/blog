86faab89b70aaf55608b6e3bc998f9ef43c4dd57
hiddenbebb <cbj4133@naver.com>
2024-06-06 23:26:10 +0900

    card img 수정

--- src/components/card/study/AI_study.js
+++ src/components/card/study/AI_study.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
-import data from "../card_name.json";
+import data from "../card_name.js";
 
 function CardItem({ title, text, link, imgSrc }) {
   // 줄바꿈 문자를 <br>로 변경
